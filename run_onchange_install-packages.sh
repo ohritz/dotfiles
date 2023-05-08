c1e2556cff591db79c42c430bdb6c4acd45ccb3b
Sohan Fernando <sohan@graceful.nu>
2023-05-08 14:52:08 +0200

    Better adding of ms apt repo

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,18 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-# Get Ubuntu version
-repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+get_version() {
+    # Get Ubuntu version
+    local repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+    echo "$repo_version"
+}
 
-# Download Microsoft signing key and repository
-wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+add_microsoft_repo()
+{
+    local version="$(get_version)"
+    # Download Microsoft signing key and repository
+    wget "https://packages.microsoft.com/config/ubuntu/${version}/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
+    
+    # Install Microsoft signing key and repository
+    sudo dpkg -i packages-microsoft-prod.deb
+    
+    # Clean up
+    rm packages-microsoft-prod.deb
+}
 
-# Install Microsoft signing key and repository
-sudo dpkg -i packages-microsoft-prod.deb
+install_packages() {
+    sudo apt-get update && \
+    sudo apt-get install -y dotnet-sdk-7.0 \
+    dotnet-sdk-6.0 \
+    gpg-agent
+}
 
-# Clean up
-rm packages-microsoft-prod.deb
-
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-7.0 \
-dotnet-sdk-6.0 \
-gpg-agent
+add_microsoft_repo
+install_packages
