cfde9662146a5c829eb087ef58185535a14ff333
Sohan Fernando <sohan@graceful.nu>
2023-05-08 08:22:42 +0200

    add ms repo

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Get Ubuntu version
+repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+
+# Download Microsoft signing key and repository
+wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+
+# Install Microsoft signing key and repository
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Clean up
+rm packages-microsoft-prod.deb
+
 sudo apt-get update && \
 sudo apt-get install -y dotnet-sdk-7.0 \
 dotnet-sdk-6.0 \
