5d0e33b0d92630734883d8ee7e0a226c5cc553c1
Sohan Fernando <sohan@graceful.nu>
2023-09-11 21:43:55 +0200

    Set linux files to only be run on linux

--- run_once_before_install-shell-lpass.sh
+++ run_once_before_install-shell-lpass.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo apt install -y zsh lastpass-cli
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
