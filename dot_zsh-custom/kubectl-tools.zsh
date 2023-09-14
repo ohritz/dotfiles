26ee8f282e57d50573e69f96150cf7d5d7170694
Sohan Fernando <sohan@graceful.nu>
2023-09-14 12:25:26 +0200

    Remove .zsh-custom/kubectl-tools.zsh Remove .zsh-custom/plugins/zsh-compinit-load/zsh-compinit-load.plugin.zsh Remove .zsh-custom/themes/amuse-remote.zsh-theme Remove .zsh-custom/themes/fernando.zsh-theme Remove .zsh-custom/themes/skipper.zsh-theme

--- dot_zsh-custom/kubectl-tools.zsh
+++
@@ -1,8 +0,0 @@
-
-_conditional_kubectx_prompt_info() {
-  local current_context=$(kubectx_prompt_info)
-
-  if [ -n "$current_context" ]; then
-    echo "${ZSH_THEME_KUBECTX_PROMT_PREFIX}${current_context}${ZSH_THEME_KUBECTX_PROMT_SUFFIX}"
-  fi
-}
